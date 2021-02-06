ad43aee5dd9230eed90cc398937d00b2796a673f
iinokaoru <39937666+iinokaoru@users.noreply.github.com>
2021-02-06 12:44:54 +0900

    client -> bot

--- discordbot.py
+++ discordbot.py
@@ -5,22 +5,22 @@ import traceback
 from urllib import request as req
 from urllib import parse
 
-client = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='/')
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
 
-@client.event
+@bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
 
-@client.command()
+@bot.command()
 async def ping(ctx):
     await ctx.send('pong')
     
-@client.command()    
+@bot.command()    
 async def nyan(ctx):
     await ctx.send('mya-')
 
